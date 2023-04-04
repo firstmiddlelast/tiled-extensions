a4647984de7676dd22a6e9e8c9ba0191ada3d0e7
firstmiddlelast <qube132@yahoo.com>
2023-04-04 13:40:01 +0200

    Comments and code layout.

--- line.mjs
+++ line.mjs
@@ -1,20 +1,31 @@
-export default function line(x0, y0, x1, y1, plot) {
-   var dx = Math.abs(x1 - x0);
-   var dy = Math.abs(y1 - y0);
-   var sx = (x0 < x1) ? 1 : -1;
-   var sy = (y0 < y1) ? 1 : -1;
-   var err = dx - dy;
+/* Draws a line. 
+ * plot (x, y) draws the point @x, y. 
+ * x and y values are not bounded in the line function, so it's up to the plot() call to check those. 
+ */
+export default function line (x0, y0, x1, y1, plot) {
+    const dx = Math.abs (x1 - x0);
+    const dy = Math.abs (y1 - y0);
+    const sx = (x0 < x1) ? 1 : -1;
+    const sy = (y0 < y1) ? 1 : -1;
+    const err = dx - dy;
 
-   while(true) {
-      plot(x0, y0); 
-      if ((x0 === x1) && (y0 === y1)) break;
+    while (true) {
+        plot (x0, y0); 
+        if ((x0 === x1) && (y0 === y1)) break;
         /*
-        Note that comparing floats directly may fail as you step (though it shouldn't when stepping by integer amounts, it might if either end point is non-integer), so instead of directly comparing the end points you might want to use an epsilon:
-        if (Math.abs(x0 - x1) < 0.0001 && Math.abs(y0 - y1) < 0.0001) break;
-        */
-      var e2 = 2*err;
-      if (e2 > -dy) { err -= dy; x0  += sx; }
-      if (e2 < dx) { err += dx; y0  += sy; }
-   }
+         * Note that comparing floats directly may fail as you step (though it shouldn't when stepping by integer amounts, it might if either end point is non-integer),
+         * so instead of directly comparing the end points you might want to use an epsilon:
+         * if (Math.abs(x0 - x1) < 0.0001 && Math.abs(y0 - y1) < 0.0001) break;
+         */
+        const e2 = 2 * err;
+        if (e2 > -dy) {
+            err -= dy; 
+            x0  += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y0  += sy;
+        }
+    }
 }
 
