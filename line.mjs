0a25bebb09f74d141c102dbc9bf7e79b58fe6bb5
firstmiddlelast <qube132@yahoo.com>
2023-04-06 17:11:52 +0200

    bugfix on the line plotting / distance measuring

--- line.mjs
+++ line.mjs
@@ -2,15 +2,25 @@
  * plot (x, y) draws the point @x, y. 
  * x and y values are not bounded in the line function, so it's up to the plot() call to check those. 
  */
-export default function line (x0, y0, x1, y1, plot, nodiagonals = true) {
+export default function line (x0, y0, x1, y1, plot, nodiagonals = false) {
     const dx = Math.abs (x1 - x0);
     const dy = Math.abs (y1 - y0);
     const sx = (x0 < x1) ? 1 : -1;
     const sy = (y0 < y1) ? 1 : -1;
     let err = dx - dy;
+    let x3;
+    let y3;
+    let x2;
+    let y2;
 
+    let lastPlottedX;
     while (true) {
-        plot (x0, y0); 
+        
+        if (!nodiagonals || (nodiagonals 
+            && (x0 !== x3 || y0 !== y3) 
+            && (x0 !== x2 || y0 !== y2))) {  // This is to ensure we don't plot an already plotted point
+            plot (x0, y0); 
+        }
         if ((x0 === x1) && (y0 === y1)) break;
         /*
          * Note that comparing floats directly may fail as you step (though it shouldn't when stepping by integer amounts, it might if either end point is non-integer),
@@ -23,6 +33,8 @@ export default function line (x0, y0, x1, y1, plot, nodiagonals = true) {
             x0  += sx;
             if (nodiagonals) {
                 plot (x0, y0);
+                x3 = x0;
+                y3 = y0;
             }
         }
         if (e2 < dx) {
@@ -30,6 +42,8 @@ export default function line (x0, y0, x1, y1, plot, nodiagonals = true) {
             y0  += sy;
             if (nodiagonals) {
                 plot (x0, y0);
+                x2 = x0;
+                y2 = y0;
             }
         }
     }
